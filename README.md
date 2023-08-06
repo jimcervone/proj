7f0676e9fff4f229aea8eb8ffec6a107e293a63f
Jim Cervone <jim@mavenmachines.com>
2023-08-06 01:19:54 -0400

    Initial commit with readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 this is my readme
 this is a reame
+this is a reame
--- projectMaker.sh
+++ projectMaker.sh
@@ -1,11 +1,15 @@
 makeProject() {
-	git init -b main
-	git remote add origin git@github.com:jimcervone/$1
-	echo "this is a reame" >> README.md
-	git add .
-	git commit -m "Initial commit with readme"
+	if git init
+	then
+		git remote add origin git@github.com:jimcervone/$1
+		echo "this is a reame" >> README.md
+		git add .
+		git commit -m "Initial commit with readme"
 	
-	# make a call to the github CLI to make the new repo
+		# make a call to the github CLI to make the new repo
 
-	git push origin main
+		git push origin master
+	else
+		echo "YOU ARE ALREADY IN A REPO!"
+	fi
 }
